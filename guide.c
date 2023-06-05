e90bf77ba52583b8fc0a1f7ca47bdc7b4feef53f
yen-cheng <e125612396@gmail.com>
2023-06-05 23:19:05 +0800

    test html

--- guide.c
+++ guide.c
@@ -6,10 +6,18 @@ int main(void)
     printf("-o 表示輸出要變成哪個檔名 \n");
 
     printf("cd 資料夾");
-    printf(" git status \n");
+    printf("git init");
+    printf("touch 開新檔案");
+    
+    //printf(" git status \n");
     printf("git add . ");
     printf("git commit -m \" {名稱} \" ");
     printf("git log 查看commit 記錄");
-
+    printf("git remote add origin https://github.com/yencheng1028/homework-code.git");
+    printf("git push -u origin master");
+    printf(" ");
+    
+    
+    
     return 0;
 }
