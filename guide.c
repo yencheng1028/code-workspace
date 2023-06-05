b8e54dbab27f0cb49bbf2df10fe57a813ee8142f
yen-cheng <e125612396@gmail.com>
2023-06-05 23:49:01 +0800

    加入指令和註解

--- guide.c
+++ guide.c
@@ -12,12 +12,14 @@ int main(void)
     //printf(" git status \n");
     printf("git add . ");
     printf("git commit -m \" {名稱} \" ");
+    printf("code . "); //開啟vscode
     printf("git log 查看commit 記錄");
+    //github create new repository
     printf("git remote add origin https://github.com/yencheng1028/homework-code.git");
+    // 遠端數據庫 名稱{master} 
     printf("git push -u origin master");
+
     printf(" ");
-    
-    
-    
+        
     return 0;
 }
