1158c5ce321ae128f890282d486b075b817f2b9d
yen-cheng <e125612396@gmail.com>
2023-06-05 21:04:25 +0800

    初始設定

---
+++ guide.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("gcc guide.c -o guide \n");
+    printf("-o 表示輸出要變成哪個檔名 \n");
+    printf("dir 顯示檔案 查看可執行檔.exe \n");
+    printf(".點 代表現行目錄 ./選哲哪個檔案 \n");
+    // printf("./guide.exe");
+
+    return 0;
+}
