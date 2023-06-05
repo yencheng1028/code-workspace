59f5fe19be406d4fdad0bf567096dfcc880b678a
yen-cheng <e125612396@gmail.com>
2023-06-05 21:39:05 +0800

    記錄教學筆記

--- guide.c
+++ guide.c
@@ -4,9 +4,12 @@ int main(void)
 {
     printf("gcc guide.c -o guide \n");
     printf("-o 表示輸出要變成哪個檔名 \n");
-    printf("dir 顯示檔案 查看可執行檔.exe \n");
-    printf(".點 代表現行目錄 ./選哲哪個檔案 \n");
-    // printf("./guide.exe");
+
+    printf("cd 資料夾");
+    printf(" git status \n");
+    printf("git add . ");
+    printf("git commit -m \" {名稱} \" ");
+    printf("git log 查看commit 記錄");
 
     return 0;
 }
