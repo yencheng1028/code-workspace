4b06663b6d159026a5d73fca0a352b1fcec52321
yen-cheng <e125612396@gmail.com>
2023-06-06 14:28:41 +0800

    加入更多指令

--- guide.c
+++ guide.c
@@ -8,16 +8,22 @@ int main(void)
     printf("cd 資料夾");
     printf("git init");
     printf("touch 開新檔案");
-    
-    //printf(" git status \n");
+    //printf("code . "); // 開啟vscode
+    // printf(" git status \n");
     printf("git add . ");
     printf("git commit -m \" {名稱} \" ");
-    printf("code . "); //開啟vscode
+    
     printf("git log 查看commit 記錄");
     //github create new repository
-    printf("git remote add origin https://github.com/yencheng1028/homework-code.git");
-    // 遠端數據庫 名稱{master} 
+    printf("註冊遠端儲存庫: git remote add origin {遠端儲存庫網址}");
     printf("git push -u origin master");
+    /*
+        更新資料到遠端 master 分支：git push -u origin master
+        -u 是指他預設會推到哪個遠端數據庫服務
+        origin 可以改它的遠端數據庫名稱，例如 git push -u github69 master
+    */
+    printf("git log ");
+    printf("git checkout {編號} ");
 
     printf(" ");
         
