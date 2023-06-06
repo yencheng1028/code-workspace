5671e1561db7c877f9fbb2b6fa0c7f892449fc48
yen-cheng <e125612396@gmail.com>
2023-06-06 17:57:47 +0800

    記錄到18.練習回顧

--- guide.c
+++ guide.c
@@ -3,30 +3,44 @@
 int main(void)
 {
     printf("gcc guide.c -o guide \n");
-    printf("-o 表示輸出要變成哪個檔名 \n");
+
+// -o 表示輸出要變成哪個檔名
 
     printf("cd 資料夾");
     printf("git init");
-    printf("touch 開新檔案");
-    //printf("code . "); // 開啟vscode
-    // printf(" git status \n");
+    printf("touch 新檔名");
+
+//printf("code . "); // 開啟vscode
+//printf(" git status \n");
+
     printf("git add . ");
-    printf("git commit -m \" {名稱} \" ");
-    
-    printf("git log 查看commit 記錄");
-    //github create new repository
-    printf("註冊遠端儲存庫: git remote add origin {遠端儲存庫網址}");
+    printf("git add [檔名] ");
+    // 加入索引目錄 .加全部檔案
+
+    printf("git commit -m [名稱] ");
+    printf("git log ");
+
+// 查看commit 記錄
+//  github create new repository
+//  註冊遠端儲存庫
+
+    printf(" git remote add origin {遠端儲存庫網址}");
     printf("git push -u origin master");
-    /*
+
+/*
         更新資料到遠端 master 分支：git push -u origin master
         -u 是指他預設會推到哪個遠端數據庫服務
         origin 可以改它的遠端數據庫名稱，例如 git push -u github69 master
-    */
+*/
     printf("git log ");
-    printf("git checkout {編號} ");
-    printf(" git checkout master");
-    
+    printf("git checkout {編號} "); // 回頭觀看版本內容
+    printf(" git checkout master(分支名稱)"); // 返回最新的版本
     printf(" ");
-        
+    printf(" ");
+
+
+
+    printf(" ");
+
     return 0;
 }
